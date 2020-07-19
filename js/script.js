cf5687c5480f9b14972b32b81542cdcbfe510a95
Chris Jol <jol.chris2@gmail.com>
2020-07-19 15:05:59 -0700

    major updates

--- js/script.js
+++ js/script.js
@@ -1,41 +1,123 @@
-let about = document.querySelector(".about")
-let services = document.querySelector(".services")
-let packages = document.querySelector(".packages")
-
-let aboutNav = document.querySelector(".about_nav")
-let servicesNav = document.querySelector(".services_nav")
-let packagesNav = document.querySelector(".packages_nav")
+let pages = document.querySelectorAll(".page")
+let navs = document.querySelectorAll(".nav")
+let services = document.querySelectorAll(".service")
+let packages = document.querySelectorAll(".package")
 
 let yOffset = window.pageYOffset
+let packageOffset = 0
 
-console.log(about.offsetTop)
-console.log(about.offsetTop + about.offsetHeight)
+let mobile = false
+let media = window.matchMedia("(max-width: 750px)")
 
 document.addEventListener("scroll", function(){
     yOffset = window.pageYOffset
 
-    setNavPosition(yOffset, about, aboutNav)
-    setNavPosition(yOffset, services, servicesNav)
-    setNavPosition(yOffset, packages, packagesNav)
+    setNavPosition(yOffset, pages, navs)
+    if(media.matches)
+        loadNav(yOffset, pages)
 })
 
-function setNavPosition(yOffset, element, elementNav){
-    if(yOffset >= element.offsetTop && yOffset < element.offsetTop + element.offsetHeight - elementNav.offsetHeight)
+for(let i = 0; i < services.length; i++)
+{
+    let service = services[i]
+    let plus = service.children[0].children[1]
+    let serviceDescription = service.children[1]
+
+    service.addEventListener("click", function(){
+        if(media.matches)
+        {
+            if(!serviceDescription.classList.contains("open"))
+            {
+                serviceDescription.classList.add("open")
+                plus.innerHTML = "x"
+            }
+            else
+            {
+                serviceDescription.classList.remove("open")
+                plus.innerHTML = "+"
+            }
+        }
+    })
+}
+
+for(let i = 0; i < packages.length; i++)
+{
+    let package = packages[i]
+
+    package.addEventListener("click", function(){
+        if(package.classList.contains("custom") && !package.classList.contains("selected") && media.matches)
+        {
+            package.classList.add("selected")
+            packages[1].classList.remove("selected")
+            packages[1].classList.add("package_clicked")
+            packages[2].classList.add("package_clicked")
+        }
+        else if(package.classList.contains("basic") && !package.classList.contains("selected") && media.matches)
+        {
+            packages[0].classList.remove("selected")
+            package.classList.add("selected")
+            package.classList.remove("package_clicked")
+            packages[2].classList.add("package_clicked")
+        }
+        else{
+            packages[0].classList.remove("selected")
+            packages[1].classList.remove("selected")
+            packages[1].classList.remove("package_clicked")
+            packages[2].classList.remove("package_clicked")
+        }
+    })
+}
+
+function setNavPosition(yOffset, elementList, navList){
+
+    let element, nav
+    for(let i = 0; i < elementList.length; i++)
     {
-        elementNav.style.position = "fixed"
-        elementNav.style.top = "0"
-        elementNav.style.bottom = "auto"
+        element = elementList[i+1]
+        nav = navList[i]
+
+        if(element != null & nav != null)
+        {
+            let fixed = nav.classList.contains("fixed")
+
+            if(yOffset >= element.offsetTop && yOffset < element.offsetTop + element.offsetHeight - nav.offsetHeight && !fixed)
+            {
+                nav.classList.remove("bottom")
+                nav.classList.add("fixed")
+            }
+            else if(yOffset < element.offsetTop && fixed)
+            {
+                nav.classList.remove("fixed")
+                nav.classList.remove("bottom")
+            }
+            else if(yOffset >= element.offsetTop + element.offsetHeight - nav.offsetHeight && fixed && media.matches)
+            {
+                nav.classList.remove("fixed")
+                nav.classList.add("bottom")
+            }
+        }
     }
-    else if(yOffset < element.offsetTop)
+}
+
+function loadNav(yOffset, pageList){
+    for(let i = 1; i < pageList.length; i++)
     {
-        elementNav.style.position = "absolute"
-        elementNav.style.top = "0"
-        elementNav.style.bottom = "auto"
-    }
-    else if(yOffset >= element.offsetTop + element.offsetHeight - elementNav.offsetHeight)
-    {
-        elementNav.style.position = "absolute"
-        elementNav.style.top = "auto"
-        elementNav.style.bottom = "0"
+        let page = pageList[i]
+        let navLoader = page.querySelector(".nav").querySelector(".nav_load-bar")
+
+        let duration = (yOffset - page.offsetTop) / page.offsetHeight
+
+        if(duration <= 0)
+        {
+            navLoader.style.width = 0
+        }
+        else if(duration > 0)
+        {
+            navLoader.style.width = `${duration * 100}%`
+        }
+        else
+        {
+            navLoader.style.width = '100%'
+        }
     }
 }
